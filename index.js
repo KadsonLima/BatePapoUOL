4dcb0217999c0fc15aaa79846528e22825abed8d
kadsonlima <kadson-kaio@hotmail.com>
2022-04-12 13:08:02 -0300

    feat : começando as funções

---
+++ index.js
@@ -0,0 +1,24 @@
+const conversa = document.querySelector(".conversa")
+
+for (i = 0; i < 5; i++) {
+    conversa.innerHTML += 'op'
+}
+
+const people = document.querySelector(".header ion-icon")
+
+people.addEventListener('click', e => {
+    retorn('https://rickandmortyapi.com/api/character/118')
+});
+
+
+
+
+function retorn(url) {
+    axios.get(url)
+    .then(response => {
+
+    })
+    .catch((err) => {
+        console.log("Erro no Api :" + err)
+    })
+}
